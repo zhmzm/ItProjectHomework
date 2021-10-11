b3b0f06d55ff039b16f595bbc2a582f691b53a83
william <979084343@qq.com>
2021-10-11 09:49:36 +0800

    redis

---
+++ back/src/main/java/com/shop/demo/controller/RedisTestController.java
@@ -0,0 +1,26 @@
+package com.shop.demo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+
+@RestController
+@RequestMapping("/redisTest")
+public class RedisTestController {
+    @Resource
+    private RedisTemplate redisTemplate;
+
+    @GetMapping
+    public String testRedis() {
+        //设置值到redis
+        redisTemplate.opsForValue().set("name","lucy");
+        //从redis获取值
+        String name = (String)redisTemplate.opsForValue().get("name");
+        return name;
+    }
+}
+
