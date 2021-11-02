17e65c56db7565ca463f2b647835e9d4167eb7a2
zhmzm <365460860@qq.com>
2021-11-02 08:59:00 +0800

    Update swaggerController.java

--- back/src/main/java/com/shop/demo/controller/swaggerController.java
+++ back/src/main/java/com/shop/demo/controller/swaggerController.java
@@ -2,6 +2,7 @@ package com.shop.demo.controller;
 
 import io.swagger.annotations.Api;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
@@ -9,7 +10,7 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 @Api(value="swagger转跳",tags={"别看我我没用"})
 public class swaggerController {
-    @RequestMapping("/")
+    @GetMapping("/")
     public ModelAndView index() {
         ModelAndView modelAndView = new ModelAndView("redirect:" + "/swagger-ui.html");
         return modelAndView;
